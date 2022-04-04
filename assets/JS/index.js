89d409872c7fe3136a15c2bef30fa6d1e29fd398
Kseniia Kovalenko <kseniiakovalenko.94@gmail.com>
2022-04-04 11:37:39 +0300

    Add freight wagon

--- assets/JS/index.js
+++ assets/JS/index.js
@@ -1,1 +1,32 @@
 "use strict";
+
+// грузовой вагон
+class FreightWagon {
+    constructor(volume, wagonType) {
+        this.volume = {
+            value: volume,
+            unit: 'm3'
+        };
+        this.wagonType = wagonType;
+        this.cargo;
+    };
+    unloadCargo() {
+        this.cargo = null;
+    };
+
+    loadCargo(...myCargo) {
+        this.cargo = myCargo;
+    };
+};
+
+class Locomotive {
+    constructor() {
+        
+    }
+}
+
+const myWagon = new FreightWagon(150, 'covered');
+
+myWagon.loadCargo('bag1', 'bag2', 'bag3');
+
+console.log(myWagon);

6ab44cda9dd0b2f026b759691e4789fb7282e83a
Kseniia Kovalenko <kseniiakovalenko.94@gmail.com>
2022-04-04 12:12:07 +0300

    Add class Locomotive

--- assets/JS/index.js
+++ assets/JS/index.js
@@ -18,15 +18,39 @@ class FreightWagon {
         this.cargo = myCargo;
     };
 };
-
+// локомотив
 class Locomotive {
-    constructor() {
-        
-    }
-}
+    constructor(maxSeats, engineType, enginePower) {
+        this.maxSeats = maxSeats;
+        this.arrayPeople = [];
+        this.engineType = engineType;
+        this.enginePower = enginePower;
+        this.isTheEngineRunning = false;
+    };
+    engineStart() {
+        if(this.arrayPeople.length !== 0) {
+            this.isTheEngineRunning = true;
+        } else {
+            console.log('Starting the engine is not possible until there is at least 1 passenger.');
+        };
+    };
+    putAPassengerOn(passenger) {
+        if(this.arrayPeople.length < this.maxSeats) {
+            this.arrayPeople.push(passenger);
+        } else {
+            console.log('The maximum number of passengers has been reached.');
+        };
+    };
+};
 
 const myWagon = new FreightWagon(150, 'covered');
-
 myWagon.loadCargo('bag1', 'bag2', 'bag3');
 
-console.log(myWagon);
+const myLocomotive = new Locomotive(3, '2TЭ116', '2x3060 l.from.');
+myLocomotive.putAPassengerOn('John');
+myLocomotive.putAPassengerOn('Bill');
+myLocomotive.putAPassengerOn('Teddy');
+
+myLocomotive.engineStart();
+
+console.log(myLocomotive);
